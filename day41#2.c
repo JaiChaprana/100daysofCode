a37495afec52fd02cf042d2f7816557277b5d3a8
Jai Chaprana <jaychaprana@gmail.com>
2025-10-19 21:44:52 +0530

    Day 42

---
+++ day41#2.c
@@ -0,0 +1,25 @@
+/* Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char c[100];
+    printf("Enter a string: ");
+    fgets(c,sizeof(c),stdin);
+
+    for (int i=0;c[i]!='\0';i++) {
+        printf("%c\n",c[i]);
+    }
+
+    return 0;
+}
---
+++ day41.c
@@ -0,0 +1,33 @@
+/* Q81: Count characters in a string without using built-in length functions.
+
+/*
+Sample Test Cases:
+Input 1:
+Hello
+Output 1:
+5
+
+Input 2:
+
+Output 2:
+1
+
+*/
+
+#include<stdio.h>
+
+int main(){
+    char c[100];
+
+    printf("Enter a string: \t");
+    fgets(c, sizeof(c), stdin);
+
+    int length=0;
+
+    for (int i=0; c[i]!='\0'; i++)
+        length++;
+
+    printf("Length of the given string is: %d\n",length);
+
+    return 0;
+}
